d574357d3b2f78d611031508aa9072cfc5b3615e
Jyoti R Sulakhe <122368666+Jyotisulakhe@users.noreply.github.com>
2023-02-22 20:37:51 +0530

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{  //check given no is natural or not
+   int i;
+   printf("enter no");
+   scanf("%d",&i);
+    if( i>=1){
+        printf("its natural number");
+    }
+    else{
+        printf("its not natural number");
+    }
+}
