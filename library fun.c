c5ae749b8b777d87bc66b1a91c354749b92b94c3
Jyoti R Sulakhe <122368666+Jyotisulakhe@users.noreply.github.com>
2023-02-27 09:37:16 +0530

    Add files via upload

---
+++ library fun.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+void calculateprice(float value);
+
+int main()
+{
+  int num;
+  printf("enter num value");
+  scanf("%d",&num);
+  printf("%f",pow(num,2));
+  return 0;
+
+}
